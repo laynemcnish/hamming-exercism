a051e4d4fb3696ab1142766d1a85ce7b82036d0a
Layne McNish <layne.mcnish@legitscript.com>
2015-08-17 11:07:11 -0700

    Changed equal signs to eql? method for clarity

--- hamming.rb
+++ hamming.rb
@@ -1,11 +1,11 @@
 class Hamming
 
   def self.compute(strand1, strand2)
-    raise ArgumentError unless strand1.length == strand2.length
+    raise ArgumentError unless strand1.length.eql? strand2.length
     char_array = strand1.split("").zip(strand2.split(""))
     hamming_distance = 0
 
-    char_array.map { |chars| hamming_distance += 1 unless chars[0] == chars[1] }
+    char_array.map { |chars| hamming_distance += 1 unless chars[0].eql? chars[1] }
     hamming_distance
   end
 end
